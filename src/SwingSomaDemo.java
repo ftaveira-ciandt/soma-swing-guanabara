5d0ca9004e2f496e3532dda85267a5e2aab37542
Francisco Taveira <ftaveira@ciandt.com>
2023-02-03 16:50:11 -0300

    [Curso06] - Soma Swing Java

--- src/SwingSomaDemo.java
+++ src/SwingSomaDemo.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class SwingSomaDemo {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SwingSomaDemo extends JFrame {
+
+
+    private JPanel panelMain;
+
+    private JTextField txtN1;
+
+    private JTextField txtN2;
+
+    private JButton btnSoma;
+
+    private JLabel lblResultado;
+
+    public SwingSomaDemo() {
+        btnSoma.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                int n1 = Integer.parseInt(txtN1.getText());
+                int n2 = Integer.parseInt(txtN2.getText());
+                int s  = n1 + n2;
+                lblResultado.setText(Integer.toString(s));
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        SwingSomaDemo h = new SwingSomaDemo();
+        h.setContentPane(h.panelMain);
+        h.setTitle("Swing Soma");
+        h.setSize(300, 400);
+        h.setVisible(true);
+        h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
